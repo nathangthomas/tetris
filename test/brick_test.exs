a3f1b2a2603b6e809eabb28de6e9b97337ca5c47
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-03 00:55:48 -0600

    Add test for block shapes and translate function

--- test/brick_test.exs
+++ test/brick_test.exs
@@ -2,14 +2,11 @@ defmodule BrickTest do
   use ExUnit.Case
 
   import Tetris.Brick
+  alias Tetris.Points
 
   test "Creats a new brick" do
     assert new_brick().name == :i
   end
-
-  # def new_brick do
-  #   new()
-  # end
 
   test "Creates a new random brick" do
     actual = new_random()
@@ -33,5 +30,33 @@ defmodule BrickTest do
         assert actual.rotation == 180
   end
 
-  def new_brick, do: new()
+  test "should return points for the i shape" do
+    points =
+      new_brick(name: :i)
+      |> shape
+
+    assert {2, 2} in points
+  end
+
+  test "should return points for the o shape" do
+    points =
+      new_brick(name: :o)
+      |> shape()
+
+    assert {3,3} in points
+  end
+
+  test "should translae a list of points" do
+    points =
+      new_brick()
+      |> shape
+      |> Points.translate({1, 1})
+      |> Points.translate({0, 1})
+
+    assert points == [{3, 3}, {3,4}, {3,5}, {3,6}]
+
+  end
+
+  def new_brick(attributes \\ []), do: new(attributes)
+
 end
