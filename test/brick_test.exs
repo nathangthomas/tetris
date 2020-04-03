1c7caf2f1b56a5e530e721cebd86def6a2d0e96d
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-02 23:02:58 -0600

    Add tests for brick movement functions

--- test/brick_test.exs
+++ test/brick_test.exs
@@ -11,13 +11,27 @@ defmodule BrickTest do
   #   new()
   # end
 
-test "Creates a new random brick" do
-  actual = new_random()
+  test "Creates a new random brick" do
+    actual = new_random()
 
-  assert actual.name in [:i, :l, :z, :t, :o]
-  assert actual.rotation in [0, 90, 180, 270]
-  assert actual.reflection in [true, false]
-end
+    assert actual.name in [:i, :l, :z, :t, :o]
+    assert actual.rotation in [0, 90, 180, 270]
+    assert actual.reflection in [true, false]
+  end
+
+  test "should manipulate brick" do
+    actual =
+      new_brick()
+        |> left
+        |> right
+        |> right
+        |> down
+        |> spin_90
+        |> spin_90
+
+        assert actual.location == {41, 1}
+        assert actual.rotation == 180
+  end
 
   def new_brick, do: new()
 end
