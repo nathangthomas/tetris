78fe0bd0b509c6970a967f72948a237188b8f648
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-10 13:57:20 -0600

    Add test for flip, rotate, and mirror

--- test/brick_test.exs
+++ test/brick_test.exs
@@ -57,6 +57,23 @@ defmodule BrickTest do
 
   end
 
+  test "should flip, rotate, and mirror" do
+    [{1, 1}]
+    |> Points.mirror
+    |> assert_point({4,1})
+    |> Points.flip
+    |> assert_point({4,4})
+    |> Points.rotate_90
+    |> assert_point({1,4})
+    |> Points.rotate_90
+    |> assert_point({1,1})
+  end
+
   def new_brick(attributes \\ []), do: new(attributes)
+
+  def assert_point([actual], expected) do
+    assert actual == expected
+    [actual]
+  end
 
 end
