7cbe64744d9afcce16571cfcfe1d6eefc799334b
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-04-09 23:09:06 -0600

    Add transpose method

--- lib/points.ex
+++ lib/points.ex
@@ -2,4 +2,15 @@ defmodule Tetris.Points do
   def translate(points, {x, y}) do
     Enum.map(points, fn {dx, dy} -> {dx + x, dy + y} end  )
   end
+
+  def transpose(points) do
+    points
+    |> Enum.map(fn {x, y} -> {y, x} end )
+  end
+
+  #defp is how you define private methods
+  # defp traspose_point({x,y}), do:{y, x}
+  # defp mirror_point({x,y}), do: {4-x, y}
+  # defp flip_point({x,y}), do: {x, 4-y}
+  #
 end
